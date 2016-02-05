2127687b3ad3cc418c80529b8afd1370bbfd1c72
Bruno Cascio <brunocascio@gmail.com>
2016-02-05 12:43:26 -0300

    Page Policies

--- wp-content/themes/clorox/page-nuestra-historia.php
+++ wp-content/themes/clorox/page-nuestra-historia.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
-  <section class="bg-green section" id="section-own history">
+  <section class="bg-green section" id="section-own-history">
     <article class="container850">
       <div class="bg-image featured-image border-x4-bottom border-blue" style="background-image: url(<?php get_featured_image_uri(get_the_ID()); ?>)"></div>
       <div class="bg-white container content shadow">
