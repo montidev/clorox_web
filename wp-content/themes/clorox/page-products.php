f028853f8730e2bddeb170ca3dfea46fefa227f2
Bruno Cascio <brunocascio@gmail.com>
2016-02-11 11:41:04 -0300

    Added page products

---
+++ wp-content/themes/clorox/page-products.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<section class="bg-blue section "id="section-products">
+  <div class="container title text-center with-filters">
+    <h2 class="clearfix">
+      <div class="col-md-5 text-right label-filter">Necesito un producto para</div>
+      <div class="col-md-3 text-left"><?php get_filter_product_types_form(); ?></div>
+      <div class="col-md-1 text-center label-filter">mi</div>
+      <div class="col-md-2 text-left"><?php get_filter_product_categories_form(); ?></div>
+    </h2>
+  </div>
+  <div class="container-products">
+    <?php display_grid_products(15); ?>
+  </div>
+</section>
+
+<?php get_footer(); ?>
