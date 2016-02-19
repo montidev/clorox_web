81377a8333d7e639a367a194bfb4cee2da96de9d
Bruno Cascio <brunocascio@gmail.com>
2016-02-19 14:53:47 -0300

    Added placeholder for multisite

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -79,6 +79,15 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+/* inicio multisite */
+define('WP_ALLOW_MULTISITE', true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'domain.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
