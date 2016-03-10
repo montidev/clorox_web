2f5e1f5071175161f773b121e08f0ffe17216264
Bruno Cascio <brunocascio@gmail.com>
2016-03-10 11:50:49 -0300

    Environments hover and cursor

--- wp-content/themes/clorox/partials/grid-categories.php
+++ wp-content/themes/clorox/partials/grid-categories.php
@@ -2,16 +2,16 @@
   <?php $categories = get_categories(array('number' => 6)); ?>
   <?php foreach ($categories as $category): ?>
     <div class="col-lg-2 col-md-4 col-xs-4">
-      <article class="box box-white box-shadow border-x4-bottom border-blue categoryBox animate-scale">
-        <div class="header">
-          <div class="bg-image" style="background-image:url(<?php get_category_image_uri($category->term_id); ?>)"></div>
-        </div>
-        <div class="body text-center">
-          <a href="<?php echo_safe(get_category_link($category->cat_ID)); ?>">
+      <a href="<?php echo_safe(get_category_link($category->cat_ID)); ?>">
+        <article class="box box-white box-shadow border-x4-bottom border-blue categoryBox animate-scale">
+          <div class="header">
+            <div class="bg-image" style="background-image:url(<?php get_category_image_uri($category->term_id); ?>)"></div>
+          </div>
+          <div class="body text-center">
             <?php echo_safe($category->name); ?>
-          </a>
-        </div>
-      </article>
+          </div>
+        </article>
+      </a>
     </div>
   <?php endforeach; ?>
 </div>
